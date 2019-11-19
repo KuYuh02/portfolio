7f70cf6ff9cac4b2c138c60926fdac5ff61b4c04
KuYuh02 <mangelodeguzman20@gmail.com>
2019-11-19 16:26:20 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <h1>Martin DeGuzman's Online Portfolio Period 1</h1></br>
+  </head>
+  <body>
+      <a href="index.html">index</a></li>
+      <a href="DeGuzman_AboutMe.html">About Me</a>
+      <a href="DeGuzman_Projects.html">Projects</a>
+      <a href="DeGuzman_ContactMe.html">Contact Me</a>
+    </body>
